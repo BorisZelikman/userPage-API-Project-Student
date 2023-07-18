3422a895be5ab6da682373dbdc4418057228c097
BorisZelikman <oborzelik@gmail.com>
2023-07-18 15:33:32 +0300

    save/load using Blob

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-// const user = { picUrl: "", name: "Name", place: "Place" };
-
-// const source = $("#user-template").html();
-// const template = Handlebars.compile(source);
-// const userHTML = template(user);
-
 const apiManager = new APIManager();
 const renderer = new Renderer();
 
@@ -20,8 +14,32 @@ const generateUser = async function () {
   console.log("rendered");
 };
 
+const savePage = (data, fileName) => {
+  let a = document.createElement("a");
+  let file = new Blob([JSON.stringify(data)], { type: "application/json" });
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+};
+
+const loadPage = () => {
+  fetch("saved-data/page.json")
+    .then((response) => response.json())
+    .then((data) => {
+      apiManager.data = data;
+      renderData();
+    })
+    .catch((error) => {
+      console.error("Error reading file:", error);
+    });
+};
 $("button").on("click", function (sender) {
   if (sender.target.id === "btnGenerate") generateUser();
   if (sender.target.id === "btnLoad") loadData();
   if (sender.target.id === "btnDisplay") renderData();
+  if (sender.target.id === "btnLoadPage") loadPage();
+  if (sender.target.id === "btnSavePage")
+    savePage(apiManager.data, "page.json");
 });
+
+generateUser();
